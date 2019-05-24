4ee2883a90047b90ea655f699f1ed9abb8abac57
Siddharth <siddharthlodha42@gmail.com>
2019-05-24 18:34:22 +0530

    changes

---
+++ register_supplier.js
@@ -0,0 +1,31 @@
+function registerSupplier() {
+
+    var supplierName = document.getElementById("supplierName").value
+    var supplierAddress = document.getElementById("supplierAddress").value
+    var supplierPhonenumber = document.getElementById("supplierPhonenumber").value
+    var supplierEmail = document.getElementById("supplierEmail").value
+
+
+    var data = JSON.stringify({
+        "supplierName": supplierName,
+        "supplierAddress":supplierAddress ,
+        "supplierPhonenumber": supplierPhonenumber,
+        "supplierEmail": supplierEmail
+    });
+
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log(this.responseText);
+        }
+    });
+
+    xhr.open("POST", "http://localhost:8080/secure-gate-pass/supplier/register");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.setRequestHeader("cache-control", "no-cache");
+
+
+    xhr.send(data);
+}
