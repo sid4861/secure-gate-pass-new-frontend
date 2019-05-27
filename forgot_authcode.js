a1ad7b240c3ce8029610eb20ecf6f72c47db6f08
Siddharth <siddharthlodha42@gmail.com>
2019-05-28 01:35:02 +0530

    ch

---
+++ forgot_authcode.js
@@ -0,0 +1,34 @@
+function retrieveAuthCode(){
+
+    var supplierEmail = document.getElementById("supplierEmail").value
+    var supplierPhonenumber = document.getElementById("supplierPhonenumber").value
+
+    var url = new URL("http://localhost:8080/secure-gate-pass/supplier/forgot-authcode?PhoneNumber=8308760642&Email=gmail.com")
+    url.searchParams.set('PhoneNumber', supplierPhonenumber)
+    url.searchParams.set('Email', supplierEmail)
+    console.log(url)
+
+    // Create a request variable and assign a new XMLHttpRequest object to it.
+    var request = new XMLHttpRequest();
+
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open('GET', url, true);
+
+    request.onload = function () {
+        // Begin accessing JSON data here
+        var data = this.response;
+        if (request.status >= 200 && request.status < 400) {
+           
+            //var para = document.createElement('p');
+            //para.innerHTML = data.value;
+            //document.getElementsByTagName('body')[0].appendChild(para)
+            
+        } else {
+            console.log('error');
+        }
+
+    }
+
+    // Send request
+    request.send();
+}
